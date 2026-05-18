9d22c74abb0157879f9b85396597e17fd5aad156
devidevpratapsingh <devidevpratapsingh@gmail.com>
2026-05-19 03:21:21 +0530

    Auth config

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -0,0 +1,14 @@
+// import { SignIn } from "@clerk/nextjs"
+
+// const SignInPage = () => {
+//   return <SignIn />
+// }
+
+// export default SignInPage
+import { SignUp } from "@clerk/nextjs";
+
+const Page = () => {
+  return <SignUp />;
+};
+
+export default Page;
